b250727718e87d7adc5ac70affd016923a40f2dc
jbradleydev <jacobbradleydev@gmail.com>
2025-01-28 11:52:01 +0000

    start of stand and dealer functionality

--- script.js
+++ script.js
@@ -4,9 +4,12 @@
 // to add to a pile https://deckofcardsapi.com/api/deck/<<deck_id>>/pile/<<pile_name>>/add/?cards=AS,2S
 
 let playerScore = 0;
+let dealerScore = 0;
 let deckId = "";
 const scoreEl = document.getElementById('results');
 let playerCards = [];
+let dealerCards = [];
+let canHit = false;
 
 async function GetDeck() {
     const response = await fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=6");
@@ -27,21 +30,32 @@ async function DrawCards(deckId, count) {
 }
 
 function CheckScore(){
+    
     if (playerScore > 21){
         playerScore = "Bust!";
         setTimeout(()=>{
             EndGame();
         }, 3000);
+    }else if(playerScore == 21){
+        //stand if blackjack
+        Stand();
+    }
+    else{
+        setTimeout(()=>{
+            canHit = true;
+        }, 300);
     }
 }
 
+//REMEMBER TO RESET ALL
 function EndGame() {
-    const body = document.querySelector('body');
     const imgs = document.querySelectorAll('img');
     playerScore = 0;
+    dealerScore = 0;
+    dealerCards = [];
     playerCards = [];
+    setTimeout(() => { StartGame(); }, 1500);
     imgs.forEach((img) => img.remove());
-    setTimeout(() => { StartGame(); }, 2000);
 }
 
 function getCardValue(card, currentScore) {
@@ -83,7 +97,6 @@ function StartGame() {
             ShowFirstDealerCards(card);
         });
     });
-
 }
 
 function ShowPlayerCards(cards) {
@@ -95,7 +108,7 @@ function ShowPlayerCards(cards) {
         img.alt = `${card.value} of ${card.suit}`;  
         img.classList.add('card-img');  
         if(playerCards.indexOf(card) > 1){
-            img.style.top = `-${playerCards.indexOf(card)*20}px`;
+            img.style.top = `-${(playerCards.indexOf(card)-1)*20}px`;
         }
         cardSlot.appendChild(img);
         playerScore += getCardValue(card, playerScore);
@@ -105,6 +118,26 @@ function ShowPlayerCards(cards) {
     scoreEl.textContent = playerScore;
 }
 
+function ShowDealerCards(cards) {
+    cards.forEach((card)=>{
+        dealerCards.push(card); 
+        dealerScore += getCardValue(card, dealerScore);
+    });
+    dealerCards.forEach((card, index) => {
+        const cardSlot = document.getElementById(`d${index + 1}`) || document.getElementById('d1'); 
+        const img = document.createElement('img'); 
+        img.src = card.image;  
+        img.alt = `${card.value} of ${card.suit}`;  
+        img.classList.add('card-img');  
+        if(dealerCards.indexOf(card) > 1){
+            img.style.top = `-${(dealerCards.indexOf(card)-1)*20}px`;
+        }
+        cardSlot.appendChild(img);
+        CheckScore();
+
+    });
+}
+
 function ShowFirstDealerCards(cards) {
     const cardSlot1 = document.getElementById('d1');
     const img = document.createElement('img'); 
@@ -112,27 +145,44 @@ function ShowFirstDealerCards(cards) {
     img.alt = `${cards[0].value} of ${cards[0].suit}`;
     img.classList.add('card-img');  
     cardSlot1.appendChild(img);
+    dealerScore += getCardValue(cards[0], dealerScore);
+    dealerCards.push(cards[0]);
 
     const cardSlot2 = document.getElementById('d2');
     const backCard = document.createElement('img');
+    backCard.id = 'backcard';
     backCard.src = 'https://deckofcardsapi.com/static/img/back.png';
     backCard.alt = 'Card Back';
     backCard.classList.add('card-img');
     cardSlot2.appendChild(backCard);
 }
 
-
-
-
 function Hit(){
+    if(canHit){
+        canHit = false;
     DrawCards(deckId, 1).then(card => {
         ShowPlayerCards(card);
         scoreEl.textContent = playerScore;
     });
 }
+}
+
+function DealerPlay(){
+    if(dealerScore < 12){
+        DrawCards(deckId, 1).then(card => {
+            ShowDealerCards(card);
+            DealerPlay();
+        });
+    }
+}
 
 function Stand(){
     //play dealers cards
+    // need dealer logic, e.g. stand on 17, less than 15 = 1/5 to hit, less than 13 = 1/3 to hit, less than 12 OR 11 or less = hit
+    // if more than 17 than 1/15 to hit
+    canHit = false;
+    DealerPlay();
+    
 }
 
 //keep track of score of both cards
